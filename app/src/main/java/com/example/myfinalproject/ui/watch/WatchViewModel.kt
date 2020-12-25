b2ff3fb959b8318099dd2c979ba8e9dd0697fe53
Tayleo <tayleo.email.com>
2020-12-25 20:55:50 +0800

    整合计时器

--- app/src/main/java/com/example/myfinalproject/ui/watch/WatchViewModel.kt
+++ app/src/main/java/com/example/myfinalproject/ui/watch/WatchViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.myfinalproject.ui.watch
 
+import android.os.Handler
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WatchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    //表示second是可变的
+    private var _second : MutableLiveData<Int> = MutableLiveData()
+    private var running =false
+    //外部不能修改，所以定义为LiveData
+    val seconds: LiveData<Int> = _second
+    init {
+        runTimer()
+    }
+
+    fun start(){
+        running=true
+    }
+
+    fun stop(){
+        running=false
+    }
+
+    fun restart(){
+        running=true
+        _second.value=0
+    }
+
+    fun runTimer(){
+        val handler =  Handler()
+        val runnable=object :Runnable{
+            override fun run() {
+                if(running){
+                    val sec=_second.value ?:0
+                    _second.value=sec+1
+                }
+                handler.postDelayed(this,1000)
+            }
+        }
+        handler.post(runnable)
+    }
 }
