b3e0abdb314a46dd70ce153369a140d031f91221
Tayleo <tayleo.email.com>
2020-12-25 20:39:05 +0800

    底部添加一个计时器导航

---
+++ app/src/main/java/com/example/myfinalproject/ui/watch/WatchFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myfinalproject.ui.watch
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myfinalproject.R
+
+class WatchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WatchFragment()
+    }
+
+    private lateinit var viewModel: WatchViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.watch_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WatchViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
